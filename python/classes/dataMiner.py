695458cedb55a3d309f5c8e8b5a6aa09ea526ee8
matiasratcliffe <mati.ratcliffe@gmail.com>
2024-01-02 15:49:25 -0300

    Data mining started

--- python/classes/dataMiner.py
+++ python/classes/dataMiner.py
@@ -1,40 +1,23 @@
 import pdb
-import etherscan
-from web3 import Web3
-from position import PositionCollection, Token
+import json
+from datetime import datetime
+from position import EVMInterface, PositionCollection, Token
 
 # HASTA LA LINEA 672 DE CAPTURED2 INCLUIDA CONTANDO HEADERS
 
 def getPairInfo(tokenInSymbol, tokenOutSymbol, transaccionHash):
     print(f"\tGetting Swap Logs...", end="")
-    swapLogs = [log for log in client.get_proxy_transaction_receipt(transaccionHash)["logs"] if len(log["data"]) == 258]
+    swapLogs = [log for log in EVMInterface.etherscanClient.get_proxy_transaction_receipt(transaccionHash)["logs"] if len(log["data"]) == 258]
     print(f"Got {len(swapLogs)} logs!")
     for log in swapLogs:
-        uniswapPair = w3.eth.contract(address=w3.to_checksum_address(log['address']), abi=pairABI)
-        token0 = w3.eth.contract(address=w3.to_checksum_address(uniswapPair.functions.token0().call()), abi=tokenABI)
-        token1 = w3.eth.contract(address=w3.to_checksum_address(uniswapPair.functions.token1().call()), abi=tokenABI)  # token1 casi siempre es WETH                    
+        uniswapPair = EVMInterface.getPairContract(address=log['address'])
+        token0 = EVMInterface.getTokenContract(address=uniswapPair.functions.token0().call())
+        token1 = EVMInterface.getTokenContract(address=uniswapPair.functions.token1().call())  # token1 casi siempre es WETH                    
         if token1.functions.symbol().call() == "WETH" and token0.functions.symbol().call() == tokenInSymbol:
             return uniswapPair.functions.token0().call(), uniswapPair.functions.token1().call(), log['address']
         elif token1.functions.symbol().call() == "WETH" and token0.functions.symbol().call() == tokenOutSymbol:
             return uniswapPair.functions.token1().call(), uniswapPair.functions.token0().call(), log['address']
     raise Exception("Error, none was WETH or pair is dissordered!")
-
-print("Initiating Web3 client...")
-rpc_url = "https://ethereum.publicnode.com"
-w3 = Web3(Web3.HTTPProvider(rpc_url))
-
-print("Initiating etherscan client...")
-api_key = "397DG3PDX5BUT6XYMVIQE518NTZQT66BZ8"
-client = etherscan.Etherscan(api_key=api_key)
-
-print("Initiating UniswapV2Router02...")
-UniswapV2Router02 = w3.eth.contract(address="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", abi=[{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsIn","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"}])
-
-tokenABI = [{"constant": True,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable": False,"stateMutability":"view","type":"function"},
-            {"constant": True,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable": False,"stateMutability":"view","type":"function"}]
-pairABI = [{"constant": True, "inputs": [], "name": "token0", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "payable": False, "stateMutability": "view", "type": "function"},
-            {"constant": True, "inputs": [], "name": "token1", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "payable": False, "stateMutability": "view", "type": "function"},
-            {"constant": True,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable": False,"stateMutability":"view","type":"function"}]
 
 def expandCSV():
     with open("capturedTransactionHashes.txt") as file:
@@ -56,39 +39,52 @@ def expandCSV():
             print(f"Exception: {e}")
             pdb.set_trace()
 
-def calculatePositions():
-    positions = PositionCollection()
-    with open("capturedTransactionHashes2.txt") as file:
+def calculatePositionsAndGas():
+    continueProcessing = True
+    processedTransactions = []
+    lastTimestampProcessed = ""
+    serializedPositions = ""
+    totalGasSpent = 0
+    if continueProcessing:
+        with open("processedTransactions.txt") as f:
+            processedTransactions = json.loads(f.read())
+        with open("saveProgress.txt") as f:
+            _data = f.read().split(',')
+            lastTimestampProcessed = _data[0]
+            totalGasSpent = float(_data[1])
+        with open("positions.txt") as f:
+            serializedPositions = f.read()
+    positions = PositionCollection(serializedPositions)
+    with open("capturedTransactionHashes.txt") as file:
         file.readline()
-        processedTransactions = []
-        totalGasSpent = 0
-        lastTimestampProcessed = ""
         try:
-            i=0
             while line := file.readline(): #TODO check this syntax!!!
+                successfulOperation = True
                 line = line.split(",")
-                if line[2] in processedTransactions:
+                uniswapPair = EVMInterface.getPairContract(address=line[7])
+                if continueProcessing and datetime.strptime(lastTimestampProcessed, '%Y-%m-%d %H:%M:%S.%f') >= datetime.strptime(line[0], '%Y-%m-%d %H:%M:%S.%f'):
                     continue
-                else:
-                    processedTransactions.append(line[2])
-                uniswapPair = w3.eth.contract(address=w3.to_checksum_address(line[7]), abi=pairABI)
                 if line[1] == "BUY":
-                    token = Token(w3.eth.contract(address=w3.to_checksum_address(line[6]), abi=tokenABI), uniswapPair, UniswapV2Router02)
+                    token = Token(EVMInterface.getTokenContract(address=line[6]), uniswapPair)
                     positions.increasePosition(token, float(line[9])/float(line[8]), float(line[8]))
-                    totalGasSpent += (int(line[10]) * int(line[11])) / 1e18
                 elif line[1] == "SELL":
-                    token = Token(w3.eth.contract(address=w3.to_checksum_address(line[5]), abi=tokenABI), uniswapPair, UniswapV2Router02)
-                    success = positions.reducePosition(token, float(line[9])/float(line[8]), float(line[8]))
-                    if success:
-                        totalGasSpent += (int(line[10]) * int(line[11])) / 1e18
+                    token = Token(EVMInterface.getTokenContract(address=line[5]), uniswapPair)
+                    print(f"SELL {line[3]}: ", end="")
+                    successfulOperation = positions.reducePosition(token, float(line[9])/float(line[8]), float(line[8]))
+                    print(successfulOperation)
+
+                if line[2] not in processedTransactions and successfulOperation:
+                    processedTransactions.append(line[2])
+                    totalGasSpent += (int(line[10]) * int(line[11])) / 1e18
                 lastTimestampProcessed = line[0]
-                i+=1
-                if i > 10:
-                    break
         finally:
-            with open("saveProgress.txt", "a") as f:
-                f.write(f"{lastTimestampProcessed} {totalGasSpent}\n")
-                #save lastTimestampProcessed and totalGasSpent and serialize positionCollection TODO
+            with open("saveProgress.txt", "w") as f:
+                f.write(f"{lastTimestampProcessed},{totalGasSpent}")
+            with open("processedTransactions.txt", "w") as f:
+                f.write(json.dumps(processedTransactions))
+            with open("positions.txt", "w") as f:
+                f.write(positions.serialize())
+    print("ENTERING INTERACTIVE CONSOLE...")
     pdb.set_trace()
 
-calculatePositions()
+calculatePositionsAndGas()
--- python/classes/position.py
+++ python/classes/position.py
@@ -1,22 +1,30 @@
+import json
+import etherscan
+from web3 import Web3
 
 
-def get_amount_in(amount_out, reserve_in, reserve_out):
-    assert amount_out > 0, 'INSUFFICIENT_OUTPUT_AMOUNT'
-    assert reserve_in > 0 and reserve_out > 0, 'INSUFFICIENT_LIQUIDITY'
+class EVMInterface:
+    w3 = Web3(Web3.HTTPProvider("https://ethereum.publicnode.com"))
+    uniswapRouter = w3.eth.contract(address="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", abi=[{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsIn","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"}])
+    etherscanClient = etherscan.Etherscan(api_key="397DG3PDX5BUT6XYMVIQE518NTZQT66BZ8")
+    tokenABI = [{"constant": True,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable": False,"stateMutability":"view","type":"function"},
+            {"constant": True,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable": False,"stateMutability":"view","type":"function"}]
+    pairABI = [{"constant": True, "inputs": [], "name": "token0", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "payable": False, "stateMutability": "view", "type": "function"},
+            {"constant": True, "inputs": [], "name": "token1", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "payable": False, "stateMutability": "view", "type": "function"},
+            {"constant": True,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable": False,"stateMutability":"view","type":"function"}]
 
-    numerator = reserve_in * amount_out * 1000
-    denominator = (reserve_out - amount_out) * 997
-
-    amount_in = (numerator // denominator) + 1
-    return amount_in
+    def getTokenContract(address):
+        return EVMInterface.w3.eth.contract(address=EVMInterface.w3.to_checksum_address(address), abi=EVMInterface.tokenABI)
+    
+    def getPairContract(address):
+        return EVMInterface.w3.eth.contract(address=EVMInterface.w3.to_checksum_address(address), abi=EVMInterface.pairABI)
 
 class Token:
-    def __init__(self, token, uniswapPair, uniswapRouter):
-        self.uniswapRouter = uniswapRouter
+    def __init__(self, token, uniswapPair):
         self.token = token
         self.address = token.address
         self.uniswapPair = uniswapPair
-    
+
     def getPriceForSpecificAmount(self, amount) -> int:
         pass
 
@@ -27,6 +35,16 @@ class Token:
         _output = self.uniswapPair.functions.getReserves().call()
         return (_output[0], _output[1])
 
+    def get_amount_in(self, amount_out, reserve_in, reserve_out):
+        assert amount_out > 0, 'INSUFFICIENT_OUTPUT_AMOUNT'
+        assert reserve_in > 0 and reserve_out > 0, 'INSUFFICIENT_LIQUIDITY'
+
+        numerator = reserve_in * amount_out * 1000
+        denominator = (reserve_out - amount_out) * 997
+
+        amount_in = (numerator // denominator) + 1
+        return amount_in
+
     def __hash__(self) -> int:
         return hash(int(self.token.address, 16))
 
@@ -34,11 +52,11 @@ class Token:
         return isinstance(__value, Token) and self.__hash__() == __value.__hash__()
 
 class Position:
-    def __init__(self, token, amount, price):
+    def __init__(self, token, amount=0, ethPutIn=0, ethGottenOut=0):
         self.token = token
         self.amount = amount
-        self.ethPutIn = amount * price  # TODO watch out for the decimals!!!
-        self.ethGottenOut = 0
+        self.ethPutIn = ethPutIn
+        self.ethGottenOut = ethGottenOut
 
     def increasePosition(self, amount, price):
         self.ethPutIn += amount * price  # TODO watch out for the decimals!!!
@@ -48,7 +66,7 @@ class Position:
         self.ethGottenOut += amount * price  # TODO watch out for the decimals!!!
         self.amount -= amount
     
-    def getPositionValue(self, considerResidue: bool):
+    def getPositionValue(self, considerResidue: bool) -> (float, float): #TODO o (float, int) ???
         residualValue = 0
         residualAmount = self.amount
         if residualAmount > 0 and considerResidue:
@@ -60,27 +78,65 @@ class Position:
                 residualValue = self.token.getPriceForSpecificAmount(availableAmount) * availableAmount
                 residualAmount -= availableAmount
         return ((self.ethGottenOut + residualValue) - self.ethPutIn, residualAmount)
+    
+    def getPositionBalance(self, considerResidue: bool) -> (float, float): #TODO o (float, int) ???
+        residualValue = 0
+        residualAmount = self.amount
+        if residualAmount > 0 and considerResidue:
+            try:
+                residualValue = self.token.getPriceForSpecificAmount(residualAmount) * residualAmount
+                residualAmount = 0
+            except:
+                availableAmount = self.token.getAvailableAmount()
+                residualValue = self.token.getPriceForSpecificAmount(availableAmount) * availableAmount
+                residualAmount -= availableAmount
+        return ((self.ethGottenOut + residualValue), residualAmount)
 
 class PositionCollection:
-    def __init__(self):
+    def __init__(self, serializedPositions: str = ""):
         self.positions = {}
+        if len(serializedPositions) > 0:
+            _positions = json.loads(serializedPositions)
+            for k, v in _positions.items():
+                token = Token(token=EVMInterface.getTokenContract(v["token"]["address"]), uniswapPair=EVMInterface.getPairContract(v["token"]["uniswapPair"]))
+                self.positions[k] = Position(token, v["amount"], v["ethPutIn"], v["ethGottenOut"])
     
     def increasePosition(self, token, amount, price):
         if token in self.positions:
             self.positions[token.address].increasePosition(amount, price)
         else:
-            self.positions[token.address] = Position(token, amount, price)
+            self.positions[token.address] = Position(token)
+            self.positions[token.address].increasePosition(amount, price)
     
     def reducePosition(self, token, amount, price) -> bool:
-        if token in self.positions:
+        if token.address in self.positions:
             self.positions[token.address].reducePosition(amount, price)
             return True
         else:
             return False
     
-    def getBalance(self, considerResidues: bool):
-        outputBalance = 0
+    def getEarnings(self, considerResidues: bool) -> float:
+        earnings = 0
         for _, position in self.positions.items():
-            outputBalance += position.getPositionValue(considerResidues)
-
-#despues sumar el gas distinguiendo por transaccion y no por swap, y tener una comparativa de los movimientos capturados a tiempo
+            earnings += position.getPositionValue(considerResidues)[0]
+        return earnings
+    
+    def getBalance(self, considerResidues: bool) -> float:
+        balance = 0
+        for _, position in self.positions.items():
+            balance += position.getPositionBalance(considerResidues)[0]
+        return balance
+    
+    def serialize(self) -> str:
+        result = {}
+        for k, v in self.positions.items():
+            result[k] = {
+                "token": {
+                    "address": v.token.address,
+                    "uniswapPair": v.token.uniswapPair.address
+                },
+                "amount": v.amount,
+                "ethPutIn": v.ethPutIn,
+                "ethGottenOut": v.ethGottenOut
+            }
+        return json.dumps(result)
